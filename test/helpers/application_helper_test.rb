03ac7a346d0c36d91c92fa4061371f37955e9029
Ying Felix Zhou <ybzhou@thoughtworks.com>
2016-10-10 22:15:21 +0800

    add test helpers to ensure helper method is exactly right

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test "full title helper" do
+    assert_equal full_title, "Ruby on Rails Sample App"
+    assert_equal full_title("Help"), "Help | Ruby on Rails Sample App"
+  end
+end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,6 +9,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
   # test "the truth" do
   #   assert true
