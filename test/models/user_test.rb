126ab1f755317476afabfa1a849a9007e5b30d6c
Ying Felix Zhou <ybzhou@thoughtworks.com>
2016-10-11 13:34:32 +0800

    Add email addresses lower-case test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -46,6 +46,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  test "email address should be saved as lower case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
@@ -55,4 +62,5 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
 end
