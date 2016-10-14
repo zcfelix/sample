063c3e7fd90bde53b70d813458c3df66312ea0e1
Ying Felix Zhou <ybzhou@thoughtworks.com>
2016-10-14 10:24:36 +0800

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -39,4 +39,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", users_path(@user), count: 0
   end
 
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_nil cookies['remember_token']
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
+  end
+
 end
