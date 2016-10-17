77f046e7e32697f6feff4981da84815ed5e8a24c
Ying Felix Zhou <ybzhou@thoughtworks.com>
2016-10-17 20:28:02 +0800

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,19 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_#{Rails.env}"
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'zcfelix-sample.heroku.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net'
+    :port => '587'
+    :authentication => :plain,
+    :user_name => ENV['SENDGRID_USERNAME']
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com'
+    :enable_starttls_auto => true
+  }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
