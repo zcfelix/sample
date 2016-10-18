3a342e6090535c8a56091ba84fc27b83963e3dac
Ying Felix Zhou <ybzhou@thoughtworks.com>
2016-10-18 20:50:10 +0800

    Add user microposts with pictures and add amazon s3 bucket

--- test/integration/micropost_interface_test.rb
+++ test/integration/micropost_interface_test.rb
@@ -9,6 +9,7 @@ class MicropostInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
 
     # Invalid submission
     assert_no_difference 'Micropost.count' do
@@ -18,9 +19,13 @@ class MicropostInterfaceTest < ActionDispatch::IntegrationTest
 
     # Valid submission
     content = "This micropsot really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: {content: content} }
+      post microposts_path, params: { micropost: {content: content,
+                                                  picture: picture } }
     end
+    micropost = assigns(:micropost)
+    assert micropost.picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
